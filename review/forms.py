1ae0e3ec8023aa18f6bdd07c9f7c422f3c2910ba
Naurah15 <naunaurahiradya@gmail.com>
2025-08-11 10:30:11 +0700

    delete some app

--- review/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from review.models import ReviewEntry
-
-class ReviewEntryForm(ModelForm):
-    class Meta:
-        model = ReviewEntry
-        fields = ["review_text"]
--- review/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from report.models import Laporan, KegiatanLaporan
-
-# Tidak membuat model baru, hanya sebagai placeholder
-# supaya app terdeteksi oleh Django
